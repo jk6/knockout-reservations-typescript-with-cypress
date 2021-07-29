6043c981be6e81a0a4f50b58dea292424c4c8ae9
Jason <jasonkelly6@gmail.com>
2021-07-29 11:32:55 -0400

    add test for remove button

--- cypress/integration/form-submit.spec.js
+++ cypress/integration/form-submit.spec.js
@@ -1,9 +1,16 @@
 describe('button events', () => {
     beforeEach(() => {
         cy.server()
-
         cy.visit('/')
     })
+
+    it ('removes a reservation row when the Remove button is clicked', () => {
+        cy.get('#removeReservation').click();
+
+        cy.wait(500).then(() => {
+            cy.get('.table tbody tr').should('have.length', 1);
+        });
+    });
 
     it('adds a reservation row when reserve button is clicked', () => {
         cy.get('#addReservation')
@@ -13,7 +20,7 @@ describe('button events', () => {
             .then(() => {
                 cy.get('.table tbody tr')
                     .should('have.length', 3)                    
-            }) 
+            });
             
         cy.get(':nth-child(3) > :nth-child(2) > .form-control')
             .contains('Standard (sandwich)')
